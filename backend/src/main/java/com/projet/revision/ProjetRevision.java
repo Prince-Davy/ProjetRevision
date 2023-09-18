08a0f47fa67c4c052195ef5c438009921480fe27
Prince-Davy <princedavy.nkakou@gmail.com>
2023-09-18 14:46:13 +0200

    First commit !

---
+++ backend/src/main/java/com/projet/revision/ProjetRevision.java
@@ -0,0 +1,15 @@
+package com.projet.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRevision {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetRevision.class, args);
+
+        System.out.println("Hello World !!");
+    }
+
+}
---
+++ backend/src/test/java/com/projet/revision/ProjetRevisionTests.java
@@ -0,0 +1,13 @@
+package com.projet.revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRevisionTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
