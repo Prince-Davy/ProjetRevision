087280a3f53c27dc3f5faf9d10d4595712e3c4cc
Prince-Davy <princedavy.nkakou@gmail.com>
2023-09-18 23:25:54 +0200

    Affichage Données en Frontend

---
+++ backend/src/main/java/com/projet/revision/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.projet.revision.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/resource")
+                .allowedOrigins("http://localhost:4200") // Autorisez uniquement cette origine
+                .allowedMethods("GET"); // Autorisez uniquement les requêtes GET
+    }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface Greeting{
+    id: string;
+    content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements  OnInit{
+  title = 'App';
+  greeting: Greeting = {id:'', content:''};
+
+  constructor(private http:HttpClient) {
+      console.log('HttpClient :', this.http);
+  }
+
+    ngOnInit(): void {
+        this.http.get<Greeting>('http://localhost:8080/resource').subscribe(
+            data => {
+                this.greeting = data
+                console.log(data)});
+  }
 }
